eab45e0aac54e64cf6f4d11d4483f2700b05d04a
HowsonLiu <948470636@qq.com>
2019-01-14 10:16:18 +0800

    添加model

--- AttachToProcessEx/ProcessInfo.cs
+++ AttachToProcessEx/ProcessInfo.cs
@@ -3,47 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Management;
 
 namespace AttachToProcessEx
 {
     /// <summary>
-    /// This is the process information we will show in listview
+    /// This is the Model we will bind
+    /// </summary>
+    class ProcessInfoModel
+    {
+        public List<ProcessInfo> Processinfolist { get; }
+
+        // Column Headers
+        public const string nameHeader = "Name";
+        public const string pidHeader = "PID";
+        public const string commandLineHeader = "Command Line";
+
+        public ProcessInfoModel()
+        {
+            Processinfolist = new List<ProcessInfo>();
+            UpdateProcessInfoList();
+        }
+
+        public void CleanProcessInfoList()
+        {
+            if(Processinfolist.Count() <= 0)
+            {
+                Processinfolist.Clear();
+            }
+        }
+
+        public void UpdateProcessInfoList()
+        {
+            CleanProcessInfoList();
+            // Need Administrator Access
+            ManagementObjectSearcher search = new ManagementObjectSearcher("select * from Win32_Process");
+            foreach(ManagementObject queryObj in search.Get())
+            {
+                PropertyDataCollection property = queryObj.Properties;
+                var name = Convert.ToString(property["Name"].Value);
+                var pid = Convert.ToString(property["ProcessID"].Value);
+                var commandline = Convert.ToString(property["CommandLine"].Value);
+                Processinfolist.Add(new ProcessInfo(name, pid, commandline));
+            }
+        }
+    }
+
+    /// <summary>
+    /// This is the process information we will show in listview single item
     /// </summary>
     class ProcessInfo
     {
-        private readonly string name;
-        private readonly string pid;
-        private readonly string commandline;
-
-        public string Name
-        {
-            get
-            {
-                return name;
-            }
-        }
-
-        public string Pid
-        {
-            get
-            {
-                return pid;
-            }
-        }
-
-        public string CommandLine
-        {
-            get
-            {
-                return commandline;
-            }
-        }
+        public string Name { get; }
+        public string Pid { get; }
+        public string CommandLine { get; }
 
         public ProcessInfo(string name, string pid,string commandline)
         {
-            this.name = name;
-            this.pid = pid;
-            this.commandline = commandline;
+            this.Name = name;
+            this.Pid = pid;
+            this.CommandLine = commandline;
         }
     }
 }
