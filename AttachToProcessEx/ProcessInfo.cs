36a8a85f68fcc814a9d3911fef1b8c0985434d61
HowsonLiu <948470636@qq.com>
2019-01-14 09:28:50 +0800

    添加进程信息类

---
+++ AttachToProcessEx/ProcessInfo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttachToProcessEx
+{
+    /// <summary>
+    /// This is the process information we will show in listview
+    /// </summary>
+    class ProcessInfo
+    {
+        private readonly string name;
+        private readonly string pid;
+        private readonly string commandline;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public string Pid
+        {
+            get
+            {
+                return pid;
+            }
+        }
+
+        public string CommandLine
+        {
+            get
+            {
+                return commandline;
+            }
+        }
+
+        public ProcessInfo(string name, string pid,string commandline)
+        {
+            this.name = name;
+            this.pid = pid;
+            this.commandline = commandline;
+        }
+    }
+}
