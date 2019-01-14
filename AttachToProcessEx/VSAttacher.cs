2f2b13829d29b47de6c1f7689f481715becdcaae
HowsonLiu <948470636@qq.com>
2019-01-14 11:36:08 +0800

    添加引用类

---
+++ AttachToProcessEx/VSAttacher.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace AttachToProcessEx
+{
+    /// <summary>
+    /// Open visual studio attach to process window and attach
+    /// </summary>
+    class VSAttacher
+    {
+        // the reference
+        private const uint LVM_FIRST = 0x1000;
+        private const uint LVM_GETITEMCOUNT = LVM_FIRST + 4;
+        private const uint LVM_GETITEMTEXT = LVM_FIRST + 115;
+        private const uint LVM_SETITEMSTATE = LVM_FIRST + 43;
+        private const uint PROCESS_VM_OPERATION = 0x0008;
+        private const uint PROCESS_VM_READ = 0x0010;
+        private const uint PROCESS_VM_WRITE = 0x0020;
+        private const uint MEM_COMMIT = 0x1000;
+        private const uint MEM_RELEASE = 0x8000;
+        private const uint MEM_RESERVE = 0x2000;
+        private const uint PAGE_READWRITE = 4;
+        private const uint LVIS_FOCUSED = 0x0001;
+        private const uint LVIS_SELECTED = 0x0002;
+        private const uint WM_LBUTTONDOWN = 0x0201;
+        private const uint WM_LBUTTONUP = 0x0202;
+
+        [DllImport("user32.dll")] private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll")] private static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpClassName, string lpWindowName);
+        [DllImport("user32.dll")] private static extern int SendMessage(IntPtr hwnd, uint Msg, int wParam, int lParam);
+        [DllImport("user32.dll")] private static extern int PostMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
+        [DllImport("user32.dll")] private static extern uint GetWindowThreadProcessId(IntPtr hwnd, out uint dwProcessId);
+        [DllImport("kernel32.dll")] private static extern IntPtr OpenProcess(uint dwDesireAccess, bool bInheritHandle, uint dwProcessId);
+        [DllImport("kernel32.dll")] private static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+        [DllImport("kernel32.dll")] private static extern bool VirtualFreeEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, uint dwfreeType);
+        [DllImport("kernel32.dll")] private static extern bool CloseHandle(IntPtr handle);
+        [DllImport("kernel32.dll")] private static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, IntPtr lpBuffer, int nSize, ref uint vNumberOfBytesRead);
+        [DllImport("kernel32.dll")] private static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, IntPtr lpBuffer, int nSize, ref uint vNumberOfBytesRead);
+
+        public struct LVITEM
+        {
+            public int mask;
+            public int iItem;
+            public int iSubItem;
+            public int state;
+            public int stateMask;
+            public IntPtr pszText;
+            public int cchTextMax;
+            public int iImage;
+            public IntPtr lParam;
+            public int iIndent;
+            public int iGroupId;
+            public int cColumns;
+            public IntPtr puColumns;
+        }
+
+        // only support chinese and english
+        private const string attachtoprocesswndnamezh_cn = "附加到进程";
+        private const string attachtoprocesswndnameen_us = "Attach to Process";
+        private const string attachbtnnamezh_cn = "附加(&A)";
+        private const string attachbtnnameen_us = "&Attach";
+
+        private const int timeout = 5;
+    }
+}
